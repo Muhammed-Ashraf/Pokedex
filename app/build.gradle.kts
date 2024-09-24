cb0febcc82658b0ff1206c72814f25a52bbe60fd
ashraf probook <ashraf8534@gmail.com>
2024-09-24 11:26:59 +0400

    buildSrc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,32 @@
+import com.example.pokedex.Configuration
 plugins {
+//    TODO like skydoves
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
+    /*
+    * Introduced in Gradle Plugin 7.0.0 to replace the applicationId or
+    * package declaration in the AndroidManifest.xml
+    * Refers to the package or the namespace where your app’s source code is located
+    *  If you have different modules in your app (like a feature module or a library module),
+    *  each module can have its own namespace,
+    * keeping code organization and build tasks modular and separate.
+    * */
     namespace = "com.example.pokedex"
     compileSdk = 34
 
     defaultConfig {
+        /*
+        * Cannot be changed once your app is published without creating a new app in the Play Store.
+        * */
         applicationId = "com.example.pokedex"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        versionCode = Configuration.versionCode
+        versionName = Configuration.versionName
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" //TODO like skydoves
     }
 
     buildTypes {
