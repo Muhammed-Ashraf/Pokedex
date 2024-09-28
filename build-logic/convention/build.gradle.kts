196fe106acc83c4e5add93a0b805a6f53ae1bd1f
ashraf probook <ashraf8534@gmail.com>
2024-09-28 23:10:19 +0400

    build-logic

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -8,3 +8,36 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.spotless.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "example.pokedex.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "example.pokedex.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "example.pokedex.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "example.pokedex.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "example.pokedex.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "example.pokedex.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+        register("spotless") {
+            id = "example.pokedex.spotless"
+            implementationClass = "SpotlessConventionPlugin"
+        }
+    }
+}
